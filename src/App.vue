91819c6e4917bec71a2e92ea4501c21d827ef847
fmabid <fmaabid@gmail.com>
2022-01-23 20:44:09 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Dropzone />
+</template>
+
+<script setup lang="ts">
+import Dropzone from "./components/dropzone.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ src/components/Dropzone.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="upBody">
+    <div>inside</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<style scoped>
+.upBody {
+  background-color: bisque;
+}
+</style>
