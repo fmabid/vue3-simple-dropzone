d1a40436acd0a7168015f86074dba12ab33f126f
fmabid <fmaabid@gmail.com>
2022-01-24 00:18:04 +0600

    on clicke selct file

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import Dropzone from "./components/dropzone.vue";
+import Dropzone from "./components/Dropzone.vue";
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-left: auto;
-  margin-right: auto;
-}
-</style>
+<style></style>
--- src/components/Dropzone.vue
+++ src/components/Dropzone.vue
@@ -1,19 +1,54 @@
 <template>
-  <div class="upBody">
-    <div>inside</div>
+  <div class="d-flex-all-center d-mainbody">
+    <div ref="refDropzone" class="d-dropzone">
+      <input ref="refFile" type="file" @change="onDrop" style="display: none" />
+
+      <div @click="refFile.click()" class="d-h-full d-flex-all-center">
+        <!-- <span v-if="isDragActive">Drop the files here ...</span>
+        <span v-else>Drag 'n' drop image here, or click to select image</span> -->
+        <p>click to select a file</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 
-defineProps<{ msg: string }>();
+// defineProps<{ msg: string }>();
+const emit = defineEmits(["onUploaded"]);
 
-const count = ref(0);
+const refFile = ref();
+const refDropzone = ref();
+
+function onDrop(acceptFiles: any) {
+  console.log(acceptFiles.target.files["0"]);
+
+  emit("onUploaded", acceptFiles.target.files["0"]);
+}
 </script>
 
 <style scoped>
-.upBody {
-  background-color: bisque;
+.d-h-full {
+  height: 100%;
+}
+.d-flex-all-center {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+.d-mainbody {
+  position: relative;
+}
+
+.d-dropzone {
+  width: 300px;
+  height: 200px;
+  background-color: rgb(224 242 254 / 1);
+  border-width: 2px;
+  border-color: rgb(125 211 252 / 1);
+  border-style: dashed;
+  border-radius: 1rem;
 }
 </style>
