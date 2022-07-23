2a0907bce1f02ac9a1e6b63e6169ecdb96cbce0b
Abid <fmaabid@gmail.com>
2022-07-24 00:57:06 +0600

    export lib components

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import Dropzone from "./Dropzone.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+
+export { Dropzone };
