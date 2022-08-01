f4f231186b9c6629ee65408fb4902fca6cb118ee
Abid <fmaabid@gmail.com>
2022-08-01 23:01:44 +0600

    congfig with vite

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+// import { createApp } from "vue";
+// import App from "./App.vue";
 
-createApp(App).mount("#app");
+// createApp(App).mount("#app");
+import Dropzone from "./Dropzone.vue";
+import "./style.css";
+
+export default Dropzone;
