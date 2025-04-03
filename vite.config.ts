ebf81144f1da2299d132805916397487b513f9c8
Abid <fmaabid@gmail.com>
2025-04-03 23:52:35 +0600

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 import vue from "@vitejs/plugin-vue";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
